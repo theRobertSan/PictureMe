95bc30b994e73208330a966522ea665aa1fa9c56
theRobertSan <robertsan.pro@gmail.com>
2022-11-20 23:17:53 +0000

    Replace current navigation method with Nav Component - Implement nav graph - Implement first dummy path for letsgo feature

--- app/src/main/java/com/example/pictureme/MainActivity.java
+++ app/src/main/java/com/example/pictureme/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -19,13 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
-
-    BottomNavigationView bottomNavigationView;
-
-    ExploreFragment exploreFragment = new ExploreFragment();
-    LetsgoFragment letsgoFragment = new LetsgoFragment();
-    HomeFragment homeFragment = new HomeFragment();
-    FriendsFragment friendsFragment = new FriendsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,30 +62,9 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setBackground(null);
         bottomNavigationView.getMenu().getItem(2).setEnabled(false);
 
-        // Set initial fragment to homeFragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+        NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
 
-        // Add on item click listener for the bottom nav bar
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.mi_explore:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, exploreFragment).commit();
-                        break;
-                    case R.id.mi_letsGo:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, letsgoFragment).commit();
-                        break;
-                    case R.id.mi_friends:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, friendsFragment).commit();
-                        break;
-                    default:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
-                        break;
-                }
-                return true;
-            }
-        });
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 
 }
--- app/src/main/java/com/example/pictureme/views/LetsgoFragment.java
+++ app/src/main/java/com/example/pictureme/views/LetsgoFragment.java
@@ -3,64 +3,36 @@ package com.example.pictureme.views;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.pictureme.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LetsgoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LetsgoFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LetsgoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LetsgoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LetsgoFragment newInstance(String param1, String param2) {
-        LetsgoFragment fragment = new LetsgoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+public class LetsgoFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_letsgo, container, false);
+        View view = inflater.inflate(R.layout.fragment_letsgo, container, false);
+
+        Button buttonEat = view.findViewById(R.id.button_eat);
+        Button buttonVisit = view.findViewById(R.id.button_visit);
+
+        buttonEat.setOnClickListener(this);
+        buttonVisit.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Navigation.findNavController(view).navigate(R.id.action_letsgoFragment_to_shakeFragment);
     }
 }

597f4d65fdae347f821e6b81af8d29a79d1a7cdb
cata <catasousa2001@gmail.com>
2023-01-15 15:53:18 +0000

    Add Filter Last 24 hours Picmes

--- app/src/main/java/com/example/pictureme/utils/FilterPicmes.kt
+++ app/src/main/java/com/example/pictureme/utils/FilterPicmes.kt
@@ -1,20 +1,31 @@
 package com.example.pictureme.utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.pictureme.data.models.Feeling
 import com.example.pictureme.data.models.Picme
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 object FilterPicmes {
+    @RequiresApi(Build.VERSION_CODES.O)
     fun getFilteredPicmes(
         picmes: List<Picme>,
         foodFeelings: List<Feeling>
     ): ArrayList<Pair<String, ArrayList<Picme>>> {
         var picmesWithFriends = ArrayList<Picme>()
         var picmesFood = ArrayList<Picme>()
+        var picmesLast24Hours = ArrayList<Picme>()
+
         var filters = ArrayList<Pair<String, ArrayList<Picme>>>()
+
         for (picme in picmes) {
+            val picmeCreatedAt = Details.getRelativeDate(picme.createdAt!!).split(" ")
+            if(picmeCreatedAt[1].equals("hours") && picmeCreatedAt[0].toInt() <= 24){
+                picmesLast24Hours.add(picme)
+            }
             if (picme.friends.isNotEmpty()) {
                 picmesWithFriends.add(picme)
             }
@@ -22,8 +33,10 @@ object FilterPicmes {
                 picmesFood.add(picme)
             }
         };
+        var last24hoursPair = Pair("Last 24 hours", picmesLast24Hours)
         var friendsPair = Pair("Your PicMe's with friends", picmesWithFriends)
         var foodPair = Pair("Food PicMes", picmesFood)
+        filters.add(last24hoursPair)
         filters.add(friendsPair)
         filters.add(foodPair)
         return filters
